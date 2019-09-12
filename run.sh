43365c6a0b92056411a3bdb6c0677cfea97dcb53
Xutai Ma <xutaima@devfair0251.h2.fair>
2019-09-12 12:17:11 -0700

    .

--- run.sh
+++ run.sh
@@ -1,6 +1,20 @@
+#/bin/bash
 set -e
+
+
+section()
+{
+    echo [$@]
+}
 base=$(dirname "$0")
+
+section backup
+old_config="~/old_config"
+mkdir -p ${old_config}
+
+
 #fzf
+section fzf
 if [ -e ~/.fzf ]
 then
     echo fzf is already installed
@@ -8,7 +22,9 @@ else
    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
    ~/.fzf/install
 fi
+
 # zsh
+section zsh
 if [ -e ~/.oh-my-zsh/custom/themes/xma.zsh-theme ]
 then
   echo Find an existing xma.zsh-theme file, mv to xma-old.zsh-theme
@@ -18,31 +34,35 @@ cp ./zsh/theme/xma.zsh-theme ~/.oh-my-zsh/custom/themes
 sed -i -e 's/ZSH_THEME=".*"/ZSH_THEME="xma"/g' ~/.zshrc
 
 # Vim
+section vim
 if [ -e ~/.vimrc ]
 then
-  echo Find an existing .vimrc file, mv to .vimrc.old
-  rm -rf ~/.vimrc.old
-  mv ~/.vimrc ~/.vimrc.old
+  echo Find an existing .vimrc file, mv to ${old_config}/.vimrc
+  mv ~/.vimrc ${old_config}.vimrc
 fi
 cp ${base}/vim/.vimrc ~
 
 if [ -e ~/.vim ]
 then
-  echo Find an existing .vim directory, mv to .vim.old
-  rm -rf ~/.vim.old
-  mv ~/.vim ~/.vim.old
+    echo Find an existing .vim directory, mv to .vim.old
+    rm -rf ${old_config}.vim
+    mv ~/.vim ${old_config}.vim
 fi
 
+mkdir -p ~/.vim
 
-mkdir -p .vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+section "vim plugins"
+echo plug.vim is alread installed
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p  ~/.vim/colors
 cd ~/.vim/colors
 git clone https://github.com/tomasr/molokai.git
 cp -s molokai/colors/molokai.vim .
+vim -c "PlugInstall | qa"
 
+section Finish
+echo Everything look nice now, enjoy!
 # tmux
 
 
